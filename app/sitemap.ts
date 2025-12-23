33b1bc99ca3cd749794235ed869c37e8fc0e033b
Manilka Navod Dikkumbura <manilka.projects@gmail.com>
2025-12-23 09:42:58 +0530

    fix: updated the sitemap generator to use the current build time as the lastModified date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,39 +12,33 @@ export default function sitemap(): MetadataRoute.Sitemap {
 	const routeInfo = [
 		{
 			path: "", // homepage
-			lastModified: new Date("2024-10-01"),
 			changeFrequency: "daily" as const,
 		},
 		{
 			path: "/about",
-			lastModified: new Date("2024-09-15"),
 			changeFrequency: "yearly" as const,
 		},
 		{
 			path: "/contact",
-			lastModified: new Date("2024-09-20"),
 			changeFrequency: "monthly" as const,
 		},
 		{
 			path: "/docs",
-			lastModified: new Date("2024-09-25"),
 			changeFrequency: "weekly" as const,
 		},
 		{
 			path: "/get-started",
-			lastModified: new Date("2024-09-30"),
 			changeFrequency: "weekly" as const,
 		},
 		{
 			path: "/pricing",
-			lastModified: new Date("2024-09-28"),
 			changeFrequency: "weekly" as const,
 		},
 	];
 
-	return routeInfo.map(({ path, lastModified, changeFrequency }) => ({
+	return routeInfo.map(({ path, changeFrequency }) => ({
 		url: path === "" ? baseUrl : `${baseUrl}${path}/`,
-		lastModified,
+		lastModified: new Date(),
 		changeFrequency,
 		priority: path === "" ? 1 : 0.8,
 	}));
